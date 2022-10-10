9232f3d81da067d39c4fb3d6f86a03e2a8db6dda
Harrison Royds <harrisonroyds@Harrisons-MacBook-Pro.local>
2022-10-10 17:17:24 +0100

    sum-digits'

--- katas/sum-digits.js
+++ katas/sum-digits.js
@@ -7,6 +7,16 @@
     --> 6
 */
 
-function sumDigits() {}
+function sumDigits(num) {
+  let numberStrings = num.toString().split("");
+  let numberStringNumbers = numberStrings.map(Number);
+  let count = 0;
+  let onlyNumbers = numberStringNumbers.forEach((numberString) => {
+    if (!isNaN(numberString)) {
+      count += numberString;
+    }
+  });
+  return count;
+}
 
 module.exports = sumDigits;
